a1e1de6d993a04f8f74e5a875053529f76d2342e
Your Name <your-email@example.com>
2025-09-05 17:30:21 +0700

    first commit

--- src/components/HN_KS24CNTT4_DoDucCuong_009.tsx
+++ src/components/HN_KS24CNTT4_DoDucCuong_009.tsx
@@ -1,7 +1,4 @@
-// import React from 'react'
-
-import { Button, Input } from "antd";
-// import Column from "antd/es/table/Column";
+import { Button, Input, message, Modal } from "antd";
 import {
   BookOpen,
   CirclePlus,
@@ -12,30 +9,91 @@ import {
   Trash,
 } from "lucide-react";
 import { useEffect, useState } from "react";
+import { v7 as uuid } from "uuid";
 
 interface vocabulary {
+  id: number | string;
   eVocabulary: string;
   vMeaning: string;
 }
+
 export default function HN_KS24CNTT4_DoDucCuong_009() {
-  const [newEVocabulary, setNewVocabulary] = useState<string>("");
+  const [newEVocabulary, setNewEVocabulary] = useState<string>("");
   const [newVMeaning, setNewVMeaning] = useState<string>("");
-  const [arrEVocabulary, setEArrVocabulary] = useState<vocabulary[]>(() => {
-    const newarr = localStorage.getItem("eVocabularys");
-    return newarr ? JSON.parse(newarr) : [];
+  const [vocabularies, setVocabularies] = useState<vocabulary[]>(() => {
+    const saved = localStorage.getItem("vocabularies");
+    return saved ? JSON.parse(saved) : [];
   });
-  const [arrVVocabulary, setArrVVocabury] = useState<vocabulary[]>(() => {
-    const newarr = localStorage.getItem("vVocabularys");
-    return newarr ? JSON.parse(newarr) : [];
-  });
-  const handleSumbit = (e: React.FormEvent) => {
-    e.preventDefault();
-  };
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {};
+
   useEffect(() => {
-    localStorage.setItem(newEVocabulary, JSON.stringify("eVocabularys"));
-    localStorage.setItem(newEVocabulary, JSON.stringify("vVocabularys"));
-  }, [arrVVocabulary, arrEVocabulary]);
+    localStorage.setItem("vocabularies", JSON.stringify(vocabularies));
+  }, [vocabularies]);
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (!newEVocabulary.trim() || !newVMeaning.trim()) {
+      message.error("Nội dung không được để trống");
+      return;
+    }
+
+    const duplicateCheck = vocabularies.some(
+      (item) => item.eVocabulary.toLowerCase() === newEVocabulary.toLowerCase()
+    );
+    if (duplicateCheck) {
+      message.error("Từ tiếng Anh này đã tồn tại trong danh sách!");
+      return;
+    }
+
+    const newWord: vocabulary = {
+      id: uuid(),
+      eVocabulary: newEVocabulary.trim(),
+      vMeaning: newVMeaning.trim(),
+    };
+
+    setVocabularies([...vocabularies, newWord]);
+    setNewEVocabulary("");
+    setNewVMeaning("");
+  };
+
+  const handleChangeVM = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setNewVMeaning(value);
+    if (!value.trim()) {
+      message.error("Nội dung không được để trống");
+    }
+  };
+
+  const handleChangeEV = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setNewEVocabulary(value);
+
+    if (!value.trim()) {
+      message.error("Nội dung không được để trống");
+      return;
+    }
+
+    const duplicateCheck = vocabularies.some(
+      (item) => item.eVocabulary.toLowerCase() === value.toLowerCase()
+    );
+    if (duplicateCheck) {
+      message.error("Từ vựng đã tồn tại");
+    }
+  };
+
+  const handleDelete = (id: number | string) => {
+    Modal.confirm({
+      title: "Xác nhận xóa",
+      content: "Bạn có chắc muốn xóa không?",
+      okText: "Xóa",
+      cancelText: "Hủy",
+      onOk: () => {
+        const newList = vocabularies.filter((v) => v.id !== id);
+        setVocabularies(newList);
+      },
+    });
+  };
+
   return (
     <>
       <div className="flex justify-center items-center w-screen h-auto border">
@@ -45,19 +103,30 @@ export default function HN_KS24CNTT4_DoDucCuong_009() {
               <BookOpen className="w-[60px] h-[35px] text-[#fbfdfb]" />
               <h1 className="text-[#fbfdfb]">Quản Lý Từ Vựng</h1>
             </div>
-            <div className="border w-[500px] h-[100px] border-[#f4f7f6] flex flex-col rounded-[5px]">
+            <div className="border w-[500px] h-auto p-2 border-[#f4f7f6] flex flex-col rounded-[5px]">
               <div className="flex m-2.5">
                 <CirclePlus className="text-[#2e8b57]" />
                 <p className="text-[#2e8b57] font-black">Thêm từ mới</p>
               </div>
-              <form className="flex m-2.5 gap-2" onSubmit={handleSumbit}>
-                <Input placeholder="Từ tiếng Anh" onChange={handleChange} />
-                <Input placeholder="Nghĩa tiếng Việt" onChange={handleChange} />
-                <Button className="bg-[#2e8b57]">Thêm</Button>
+              <form className="flex m-2.5 gap-2" onSubmit={handleSubmit}>
+                <Input
+                  placeholder="Từ tiếng Anh"
+                  value={newEVocabulary}
+                  onChange={handleChangeEV}
+                />
+                <Input
+                  placeholder="Nghĩa tiếng Việt"
+                  value={newVMeaning}
+                  onChange={handleChangeVM}
+                />
+                <Button htmlType="submit" className="bg-[#2e8b57] text-white">
+                  Thêm
+                </Button>
               </form>
             </div>
           </div>
-          <div className="border border-[#f4f7f6] rounded-2.5">
+
+          <div className="border border-[#f4f7f6] rounded-2.5 p-3">
             <div className="flex gap-1">
               <List className="text-[#2e8b57] w-[30px] h-[30px]" />
               <h3 className="text-[20px] font-black text-[#2e8b57]">
@@ -65,29 +134,38 @@ export default function HN_KS24CNTT4_DoDucCuong_009() {
               </h3>
             </div>
             <div className="mt-5">
-              <table className="flex flex-col gap-5 justify-center items-center">
-                <th className="flex gap-5 border-b-[1px] border-[#f4f7f6] bg-[#f8f9fa] w-[400px]">
-                  <td>Từ tiếng Anh</td>
-                  <td>Nghĩa tiếng Việt</td>
-                  <td>Hành Động</td>
-                </th>
-                <tr className="flex gap-22 border-[#f4f7f6] border-b-[1px]">
-                  <td>Apple</td>
-                  <td>Quả Táo</td>
-                  <td className="flex gap-2">
-                    <button className="flex border border-[#f4f7f6] bg-blue-500 w-[50px] h-[30px] justify-center items-center">
-                      <SquarePen className="text-[#ffff]" />{" "}
-                      <span className="text-[#ffff]">Sửa</span>
-                    </button>{" "}
-                    <button className="flex border border-[#f4f7f6] bg-red-500 w-[50px] h-[30px] justify-center items-center">
-                      <Trash className="text-[#ffff]" />{" "}
-                      <span className="text-[#ffff]">Xóa</span>
-                    </button>
-                  </td>
-                </tr>
+              <table className="w-full text-center">
+                <thead>
+                  <tr className="bg-[#f8f9fa] border-b-[1px] border-[#f4f7f6]">
+                    <th className=" p-2">Từ tiếng Anh</th>
+                    <th className=" p-2">Nghĩa tiếng Việt</th>
+                    <th className=" p-2">Hành Động</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {vocabularies.map((item, index) => (
+                    <tr key={index} className="border-b-[1px] border-[#f4f7f6]">
+                      <td className=" p-2">{item.eVocabulary}</td>
+                      <td className=" p-2">{item.vMeaning}</td>
+                      <td className="flex gap-2 p-2">
+                        <button className="flex border border-[#f4f7f6] bg-blue-500 w-[60px] h-[30px] justify-center items-center">
+                          <SquarePen className="text-[#fff]" />
+                          <span className="text-[#fff]">Sửa</span>
+                        </button>
+                        <button
+                          onClick={() => handleDelete(item.id)}
+                          className="flex border border-[#f4f7f6] bg-red-500 w-[60px] h-[30px] justify-center items-center"
+                        >
+                          <Trash className="text-[#fff]" />
+                          <span className="text-[#fff]">Xóa</span>
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
               </table>
             </div>
-            <div className="flex justify-center items-center gap-2.5">
+            <div className="flex justify-center items-center gap-2.5 mt-3">
               <MoveLeft className="border bg-[#f8f8f8]" />
               <div>
                 <p className="border w-[20px] text-center">1</p>
